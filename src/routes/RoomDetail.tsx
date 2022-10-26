6d183e7b9e8e5cd2a19786ebaa6a3f756cdaae43
jeongsangtae <wjdtkdxo55@naver.com>
2022-10-27 00:00:23 +0900

    #Skeleton

--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -63,41 +63,45 @@ export default function RoomDetail() {
           <AvatarBadge borderColor="white" boxSize="30px" bg="facebook.400" />
         </Avatar>
       </HStack>
-      <Box mt={10}>
-        <Heading mb={5} fontSize={"2xl"}>
-          <HStack>
-            <FaStar />
-            <Text>{data?.rating}</Text>
-            <Text>•</Text>
-            <Text>{reviewsData?.length} review{reviewsData?.length === 1 ? "" : "s"}</Text>
-          </HStack>
-        </Heading>
-        <Container mt={15} maxW="container.lg" marginX="none">
-          <Grid gap={10} templateColumns={"1fr 1fr"}>
-            {reviewsData?.map((review, index) =>
-              <VStack alignItems={"flex-start"} key={index}>
-                <HStack>
-                  <Avatar
-                    name={review.user.name}
-                    src={review.user.avatar}
-                    size="md"
-                  />
-                  <VStack spacing={0} alignItems={"flex-start"}>
-                    <Heading fontSize={"md"}>
-                      {review.user.name}
-                    </Heading>
-                    <HStack spacing={1}>
-                      <FaStar size="12px" />
-                      <Text>{review.rating}</Text>
-                    </HStack>
-                  </VStack>
-                </HStack>
+      <HStack mt={10}>
+        <Skeleton isLoaded={!isLoading} h={"30px"}>
+          <Heading mb={5} fontSize={"2xl"}>
+            <HStack>
+              <FaStar />
+              <Text>{data?.rating}</Text>
+              <Text>•</Text>
+              <Text>{reviewsData?.length} review{reviewsData?.length === 1 ? "" : "s"}</Text>
+            </HStack>
+          </Heading>
+        </Skeleton>
+      </HStack>
+      <Container mt={15} maxW="container.lg" marginX="none">
+        <Grid gap={10} templateColumns={"1fr 1fr"}>
+          {reviewsData?.map((review, index) =>
+            <VStack alignItems={"flex-start"} key={index}>
+              <HStack>
+                <Avatar
+                  name={review.user.name}
+                  src={review.user.avatar}
+                  size="md"
+                />
+                <VStack spacing={0} alignItems={"flex-start"}>
+                  <Heading fontSize={"md"}>
+                    {review.user.name}
+                  </Heading>
+                  <HStack spacing={1}>
+                    <FaStar size="12px" />
+                    <Text>{review.rating}</Text>
+                  </HStack>
+                </VStack>
+              </HStack>
+              <Skeleton isLoaded={!isLoading} w={"300px"} h={"30px"}>
                 <Text>{review.payload}</Text>
-              </VStack>
-            )}
-          </Grid>
-        </Container>
-      </Box>
+              </Skeleton>
+            </VStack>
+          )}
+        </Grid>
+      </Container>
     </Box>
   );
 }
