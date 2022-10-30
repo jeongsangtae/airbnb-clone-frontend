0647fa5d8675a41e417dce4cb2c5bf56b6f0309f
jeongsangtae <wjdtkdxo55@naver.com>
2022-10-30 23:39:32 +0900

    Kakao Confirm

--- src/routes/KakaoConfirm.tsx
+++ src/routes/KakaoConfirm.tsx
@@ -1,5 +1,5 @@
 import { Heading, Spinner, Text, useToast, VStack } from "@chakra-ui/react";
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { kakaoLogIn } from "../api";
@@ -9,26 +9,34 @@ export default function KakaoConfirm() {
   const toast = useToast();
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+  const mutation = useMutation(kakaoLogIn, {
+    onSuccess: () => {
+      toast({
+        status: "success",
+        title: "Welcome!",
+        description: "Happy to have you back!",
+      });
+
+      queryClient.refetchQueries(["me"]);
+
+      navigate("/");
+    },
+  });
+
+
   const confirmLogin = async () => {
-    const params = new URLSearchParams(search);
-    const code = params.get("code");
+    const code = new URLSearchParams(search).get("code");
+
+
     if (code) {
-      const status = await kakaoLogIn(code);
-      if (status === 200) {
-        toast({
-          status: "success",
-          title: "Welcome!",
-          position: "bottom-right",
-          description: "Happy to have you back! Kakao!",
-        });
-        queryClient.refetchQueries(["me"]);
-        navigate("/");
-      }
+      mutation.mutate(code);
     }
   };
+
   useEffect(() => {
     confirmLogin();
   }, []);
+
   return (
     <VStack justifyContent={"center"} mt={40}>
       <Heading>Processing log in...</Heading>
