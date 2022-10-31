bce26ce22b0346311d3196780a40fcac5fd0192e
jeongsangtae <wjdtkdxo55@naver.com>
2022-10-31 23:57:33 +0900

    21.0 Protected Pages

--- src/components/HostOnlyPage.tsx
+++ src/components/HostOnlyPage.tsx
@@ -2,11 +2,7 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useUser from "../lib/useUser";
 
-interface IHostOnlyPageProps {
-  children: React.ReactNode;
-}
-
-export default function HostOnlyPage({ children }: IHostOnlyPageProps) {
+export default function HostOnlyPage() {
   const { isLoggedIn, userLoading } = useUser();
   const navigate = useNavigate();
   useEffect(() => {
@@ -16,5 +12,5 @@ export default function HostOnlyPage({ children }: IHostOnlyPageProps) {
       }
     }
   }, [userLoading, isLoggedIn, navigate]);
-  return <>{children}</>;
+  return;
 }
